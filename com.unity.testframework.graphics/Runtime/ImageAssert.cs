eb94e5b6618d8d290c3aee86adcda5ea97ed081a
arnaudcarre <arnaud@unity3d.com>
2018-11-27 11:51:45 +0100

    added some comments

--- com.unity.testframework.graphics/Runtime/ImageAssert.cs
+++ com.unity.testframework.graphics/Runtime/ImageAssert.cs
@@ -49,6 +49,10 @@ namespace UnityEngine.TestTools.Graphics
             int height = settings.TargetHeight;
             var format = expected != null ? expected.format : TextureFormat.ARGB32;
 
+            // Some HDRP test fail with HDRP batcher because shaders variant are compiled "on the fly" in editor mode.
+            // Persistent PerMaterial CBUFFER is build during culling, but some nodes could use new variants and CBUFFER will be up to date next frame.
+            // ( this is editor specific, standalone player has no frame delay issue because all variants are ready at init stage )
+            // This PR adds a dummy rendered frame before doing the real rendering and compare images ( test already has frame delay, but there is no rendering )
             int dummyRenderedFrameCount = 1;
 
             var rt = RenderTexture.GetTemporary(width, height, 24);
@@ -60,9 +64,7 @@ namespace UnityEngine.TestTools.Graphics
                     foreach (var camera in cameras)
                     {
                         camera.targetTexture = rt;
-//                        Rendering.GraphicsSettings.RenderDocFrameCaptureStart();
                         camera.Render();
-//                        Rendering.GraphicsSettings.RenderDocFrameCaptureEnd();
                         camera.targetTexture = null;
                     }
 
